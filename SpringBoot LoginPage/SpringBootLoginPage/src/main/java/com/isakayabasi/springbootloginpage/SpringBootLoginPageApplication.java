d06aaa005a8c98badf67b4c82eb8ba1c3981b5ea
isa KAYABASI <103357055+ISA-KAYABASI@users.noreply.github.com>
2022-05-26 20:57:19 +0300

    Add files via upload

---
+++ SpringBoot LoginPage/SpringBootLoginPage/src/main/java/com/isakayabasi/springbootloginpage/SpringBootLoginPageApplication.java
@@ -0,0 +1,13 @@
+package com.isakayabasi.springbootloginpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoginPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLoginPageApplication.class, args);
+    }
+
+}
---
+++ SpringBoot LoginPage/SpringBootLoginPage/src/test/java/com/isakayabasi/springbootloginpage/SpringBootLoginPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isakayabasi.springbootloginpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoginPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
