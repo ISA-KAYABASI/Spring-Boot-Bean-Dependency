1cf972335cdcf46760cae14b1924b71cb9f791be
isa KAYABASI <103357055+ISA-KAYABASI@users.noreply.github.com>
2022-06-15 18:39:28 +0300

    Add files via upload

---
+++ springboot_crud_restful-webservice/src/test/java/com/isakayabasi/springboot_crud_restfulwebservice/SpringbootCrudRestfulWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isakayabasi.springboot_crud_restfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudRestfulWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
